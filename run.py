7f8a9bb04b0eeddd6f4a683b87bf745f8892870c
MarkHewitt76 <markyhewitt@gmail.com>
2022-08-30 10:41:50 +0000

    Fix initial linter-raised problems in run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+"""
+os module for getting IP and PORT
+environment variables
+"""
 import os
 from flask import Flask
 
@@ -7,11 +11,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Homepage view
+    """
     return "Hello, World!"
 
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"), 
-        port=int(os.environ.get("PORT", "5000")), 
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
